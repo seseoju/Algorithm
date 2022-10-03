37ee5dae839837a713e827e1883fbefed40203b1
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2022-10-03 12:11:08 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    if (n < 2) return 1;
+    
+    const dp = new Array(n + 1).fill(0);
+    dp[0] = 0, dp[1] = 1, dp[2] = 2;
+    
+    for (let i = 3; i < n + 1; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+    
+    return dp[n];
+}

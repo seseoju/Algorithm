fd8a9cdd2ee40b4cf6f19c531857997f7fd4bbbe
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2022-10-21 08:06:15 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,4 +1,7 @@
-const solution = (n) => {
-    if (n <= 7) return 1;
-    return n % 7 === 0 ? n / 7 : Math.trunc(n / 7) + 1;
-}
+// Math.ceil(): 주어진 숫자보다 크거나 같은 숫자중 가장 작은 정수를 반환
+const solution = (n) => Math.ceil(n / 7);
+
+// const solution = (n) => {
+//     if (n <= 7) return 1;
+//     return n % 7 === 0 ? n / 7 : Math.trunc(n / 7) + 1;
+// }

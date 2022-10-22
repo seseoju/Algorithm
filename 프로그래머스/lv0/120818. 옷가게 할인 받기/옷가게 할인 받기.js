80f8c704d1422ad8efb829d542af5ce5eb2f8816
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2022-10-22 23:56:59 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,8 +1,8 @@
 function solution(price) {
     let result = price;
     
-    if (price >= 500000) result = Math.floor(price * 0.8);
-    else if (price >= 300000) result = Math.floor(price * 0.9);
-    else if (price >= 100000) result = Math.floor(price * 0.95);
-    return result;
+    if (price >= 500000) result *= 0.8;
+    else if (price >= 300000) result *= 0.9;
+    else if (price >= 100000) result *= 0.95;
+    return Math.floor(result);
 }

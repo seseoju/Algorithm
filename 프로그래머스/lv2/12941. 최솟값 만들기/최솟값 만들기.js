72538a350f251fa22815ffb106d745d6a3578273
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2022-11-17 09:22:42 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.70 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -2,7 +2,6 @@ function solution(A,B){
     // A는 오름차순으로 정렬한다.
     // B는 내림차순으로 정렬한다.
     // A, B의 첫법째 숫자부터 차례로 곱하여 곱한 값을 누적한다.
-    let answer = 0;
     A.sort((a, b) => a - b);
     B.sort((a, b) => b - a);
     

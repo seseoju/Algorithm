37c66d8beaf2e42a2037c410588685392cf63f24
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-01-12 12:25:01 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,35 @@
+// function solution(ingredient) {
+//     const rule = "1231";
+//     let result = 0;
+//     let order = ingredient.join("");
+    
+//     while (order.includes(rule)) {
+//         order = order.replace(rule, "");
+//         result++;
+//     }
+    
+//     return result;
+// }
+
+function solution(ingredient) {
+    let result = 0;
+    const stack = [];
+    
+    for (item of ingredient) {
+        stack.push(item);
+        
+        if (stack[stack.length - 1] === 1 
+            && stack[stack.length - 2] === 3 
+            && stack[stack.length - 3] === 2 
+            && stack[stack.length - 4] === 1) 
+        {
+            stack.pop();
+            stack.pop();
+            stack.pop();
+            stack.pop();
+            result++;
+        }
+    }
+    
+    return result;
+}

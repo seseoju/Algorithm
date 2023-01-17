950c68c994def2f6fed0c88daf6efe1a35ae3c34
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-01-17 09:51:14 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -13,13 +13,21 @@
 //     return answer;
 // }
 
+// function solution(n) {
+//     let answer = 0;
+    
+//     while (n) {
+//         if (n % 2 === 1) answer++;
+//         n = Math.floor(n / 2);
+//     }
+    
+//     return answer;
+// }
+
+// 어떠한 수를 2로 나누고, 그 몫을 또 2로 계속해서 나누면서 나오는 나머지들의 모든 합은 어떠한 수를 이진수로 변환한 수의 1의 개수와 같다
 function solution(n) {
-    let answer = 0;
+    if (n === 1) return 1;
     
-    while (n) {
-        if (n % 2 === 1) answer++;
-        n = Math.floor(n / 2);
-    }
-    
-    return answer;
+    const binaryArr = [...n.toString(2)];
+    return binaryArr.reduce((acc, cur) => acc + (+cur), 0)
 }

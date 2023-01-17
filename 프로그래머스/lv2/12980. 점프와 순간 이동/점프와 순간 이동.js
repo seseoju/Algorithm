77b949577dbb63caa121893c945cfc911346f7b5
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-01-17 09:38:55 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = 0;
+    
+    while (n) {
+        if (n % 2 === 0) {
+            n /= 2;
+        } else {
+            n -= 1;
+            answer += 1;
+        }
+    }
+    
+    return answer;
+}

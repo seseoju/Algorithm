2278a97ac3aa7c0140257d3ec18c2d345e5d32b7
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-02-21 21:29:54 +0900

    [unrated] Title: 호텔 대실, Time: 8.06 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.js
@@ -1,11 +1,13 @@
 function solution(book_time) {
     if (book_time.length === 1) return 1;
 
+    // 데이터를 숫자 타입으로 변경하고, 오름차순 정렬
     const bookData = manipulateData(book_time).sort((a, b) => a[0] - b[0]);
     const room = [];
 
     for (let i = 0; i < bookData.length; i++) {
         const [start, end] = bookData[i];
+        // 청소완료시간 이후에 들어오는 손님을 찾는다.
         const checkIn = room.filter((endTime) => endTime <= start);
     
         if (!checkIn.length) {
@@ -18,13 +20,14 @@ function solution(book_time) {
     return room.length;
 }
 
+// book_time을 숫자 타입으로 변형
 function manipulateData(data) {
     const array = [];
     
     for (const [start, end] of data) {
         const newStart = Number(start.split(":").join(""));
         let newEnd = Number(end.split(":").join(""));
-        
+        // 50 ~ 59분은 50을 더해서 시간을 알맞게 변형
         newEnd += newEnd % 100 >= 50 ? 50 : 10;
         array.push([newStart, newEnd]);
     }

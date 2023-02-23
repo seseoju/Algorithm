f177015ac2969e8af49b674b089faeaa5a83322e
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-02-23 12:04:03 +0900

    Update 롤케이크 자르기.js

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -4,10 +4,12 @@ function solution(topping) {
     const older = new Map(); // 형
     const younger = new Map(); // 동생
     
+    // 형이 일단 롤케이크를 다 가져간다.
     for (const num of topping) {
         older.set(num, older.get(num) + 1 || 1);
     }
     
+    // 다시 배열을 순회하면서 현재 순회중인 토핑을 동생 Map에 저장.
     for (const num of topping) {
         if (older.has(num)) {
             older.set(num, older.get(num) - 1);
@@ -16,6 +18,7 @@ function solution(topping) {
             }
         }
         younger.set(num, younger.get(num) + 1 || 1);
+        // 형, 동생 Map 크키 같으면 동일한 가짓수의 토핑을 가진 것
         if (older.size === younger.size) answer++;
     }
     

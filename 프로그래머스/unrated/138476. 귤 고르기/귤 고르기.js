81a7d92137297f31c0ca6f14e40b7587fa62b4bc
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-02-24 09:08:40 +0900

    [unrated] Title: 귤 고르기, Time: 58.17 ms, Memory: 50.7 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,22 +1,38 @@
+// function solution(k, tangerine) {
+//     let answer = 0;
+//     const map = new Map();
+    
+//     for (let i = 0; i < tangerine.length; i++) {
+//         map.set(tangerine[i], map.get(tangerine[i]) + 1 || 1);
+//     }
+    
+//     const count = Array.from(map, ([_, value]) => value).sort((a, b) => b - a);
+//     let temp = k;
+    
+//     for (let i = 0; i < count.length; i++) {
+//         if (count[i] <= temp) {
+//             temp -= count[i];
+//         } else {
+//             temp = 0;
+//         }
+//         answer++;
+//         if (temp === 0) break;
+//     }
+    
+//     return answer;
+// }
+
 function solution(k, tangerine) {
     let answer = 0;
-    const map = new Map();
+    const obj = {};
     
-    for (let i = 0; i < tangerine.length; i++) {
-        map.set(tangerine[i], map.get(tangerine[i]) + 1 || 1);
-    }
+    tangerine.forEach((num) => obj[num] = (obj[num] || 0) + 1);
     
-    const count = Array.from(map, ([_, value]) => value).sort((a, b) => b - a);
-    let temp = k;
-    
-    for (let i = 0; i < count.length; i++) {
-        if (count[i] <= temp) {
-            temp -= count[i];
-        } else {
-            temp = 0;
-        }
+    const arr = Object.values(obj).sort((a, b) => b - a);
+    for (const num of arr) {
         answer++;
-        if (temp === 0) break;
+        if (k > num) k -= num;
+        else break;
     }
     
     return answer;

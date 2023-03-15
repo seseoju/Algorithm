9b3160b0a60d9a8aa6b32a927e1bfa1f6fa7a9d7
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-03-15 15:16:35 +0900

    [unrated] Title: 숫자 변환하기, Time: 19.61 ms, Memory: 51.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,24 @@
+function solution(x, y, n) {
+    if (x === y) return 0;
+    
+    const dp = new Array(y + 1).fill(Number.MAX_SAFE_INTEGER);
+    dp[x] = 0;
+    
+    for (let i = x; i <= y; i++) {
+        if (dp[i] === Number.MAX_SAFE_INTEGER) continue;
+        
+        if (i + n <= y) {
+            dp[i + n] = Math.min(dp[i + n], dp[i] + 1);
+        } 
+        
+        if (i * 2 <= y) {
+            dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1)
+        }
+
+        if (i * 3 <= y) {
+            dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1)
+        }
+    }
+    
+    return dp[y] === Number.MAX_SAFE_INTEGER ? -1 : dp[y];
+}

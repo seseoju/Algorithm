c7c2b39bfa2d75a00616c47a24de3fc8dbb81abd
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-03-20 12:01:51 +0900

    [unrated] Title: 미로 탈출, Time: 0.58 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.js
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.js
@@ -1,3 +1,4 @@
+// BFS로 S -> L, L -> E 까지 부분적으로 최단거리 구하기
 function solution(maps) {
     // 시작 지점(S) 찾기
     let start = [0, 0];
@@ -13,7 +14,7 @@ function solution(maps) {
     const ROW = maps.length, COL = maps[0].length;
     const count = Array.from({ length: ROW }, () => Array(COL).fill(0));
     let visited = Array.from ({length: ROW}, () => Array(COL).fill(false));
-    let isOpen = false;
+    let isOpen = false; // 레버를 당겨 문이 열렸는지 여부
     let queue = [];
     queue.push(start);
     visited[start[0]][start[1]] = true;
@@ -25,6 +26,7 @@ function solution(maps) {
         
         if (maps[r][c] === "L") {
             queue = [];
+            // S -> L까지의 최단거리 구했으므로 방문 여부 초기화
             visited = Array.from ({length: ROW}, () => Array(COL).fill(false));
             visited[r][c] = true;
             isOpen = true;
@@ -32,6 +34,7 @@ function solution(maps) {
         
         if (isOpen && maps[r][c] === "E") return count[r][c];
 
+        // 4방향 이동 가능한지 여부
         for (let i = 0; i < 4; i++) {
             const [newR, newC] = [r + dirR[i], c + dirC[i]];
             if (newR < 0 || newC < 0 || newR >= ROW || newC >= COL) continue;

c923384bfa7ca4b451caa41895d8e962afdc57b6
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-03-22 07:14:41 +0900

    [Silver III] Title: 퇴사, Time: 132 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.js
+++ 백준/Silver/14501. 퇴사/퇴사.js
@@ -3,18 +3,18 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const [N, ..._schedule] = fs.readFileSync(filePath).toString().split("\n");
 const schedule = _schedule.map((info) => info.split(" ").map(Number));
 
-// Bottom-Up
+// Top-Down
 function solution(n, schedule) {
   const dp = Array(n + 1).fill(0);
 
-  for (let i = 0; i < n; i++) {
-    for (let j = i + schedule[i][0]; j < N + 1; j++) {
-      if (dp[j] < dp[i] + schedule[i][1]) {
-        dp[j] = dp[i] + schedule[i][1];
-      }
+  for (let i = n - 1; i >= 0; i--) {
+    if (i + schedule[i][0] > n) {
+      dp[i] = dp[i + 1];
+    } else {
+      dp[i] = Math.max(dp[i + 1], schedule[i][1] + dp[i + schedule[i][0]]);
     }
   }
 
-  return dp[dp.length - 1];
+  return dp[0];
 }
 console.log(solution(Number(N), schedule));

fc33ac18e63b8333ceeac1840044418b99deb637
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-04-08 09:56:08 +0900

    [level 3] Title: 베스트앨범, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -1,13 +1,16 @@
 function solution(genres, plays) {
     let answer = [];
     
+    // 각 장르에 속한 노래들의 총 재생 횟수를 구한다.
     const total = {};
     for (let i = 0; i < genres.length; i++) {
         total[genres[i]] = (total[genres[i]] || 0) + plays[i];
     }
     
+    // 총 재생 횟수가 가장 많은 장르부터 정렬한다.
     const sortedGenres = Object.keys(total).sort((a, b) => total[b] - total[a]);
     
+    // 각 장르 내에서 많이 재생된 순으로 정렬해 2개의 곡만 남긴다.
     const playCount = {};
     for (let i = 0; i < genres.length; i++) {
         playCount[genres[i]] = (playCount[genres[i]] || []).concat(i);
@@ -18,6 +21,7 @@ function solution(genres, plays) {
         playCount[genre] = playCount[genre].slice(0, 2);
     }
     
+    // 총 재생 횟수가 가장 높은 장르부터 곡들을 나열한다.
     for (const genre of sortedGenres) {
         answer = [...answer, ...playCount[genre]];
     }

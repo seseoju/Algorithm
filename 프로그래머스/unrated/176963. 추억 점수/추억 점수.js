c8dbcdd7d4ad75539f6289d29cd9ea6ac7260f86
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-04-12 10:47:15 +0900

    [unrated] Title: 추억 점수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,18 +1,7 @@
 function solution(name, yearning, photo) {
     const score = {};
-    const result = [];
-    
     for (let i = 0; i < name.length; i++) {
         score[name[i]] = yearning[i];
     }
-    
-    for (const list of photo) {
-        const sum = list.reduce((acc, cur) => {
-            if (score[cur]) return score[cur] + acc;
-            return acc
-        }, 0);
-        result.push(sum);
-    }
-    
-    return result;
+    return photo.map(list => list.map(name => score[name] || 0).reduce((acc, cur) => acc + cur, 0));
 }

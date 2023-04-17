494d74d9395f80fb88e158491be1586b2eeed78c
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-04-18 00:06:48 +0900

    [Silver III] Title: 문자열 집합, Time: 196 ms, Memory: 33048 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.js
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.js
@@ -3,17 +3,18 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const [a, ...b] = fs.readFileSync(filePath).toString().trim().split("\n");
 const [n, m] = a.split(" ").map(Number);
 
-function solution(arr, n) {
-  const frequency = new Map();
-  for (const str of arr) {
-    frequency.set(str, (frequency.get(str) || 0) + 1);
+function solution(arr, n, m) {
+  const s = new Set();
+  let answer = 0;
+
+  for (let i = 0; i < n; i++) {
+    s.add(arr[i]);
   }
 
-  const s = arr.slice(0, n);
-  return s.reduce((acc, cur) => {
-    if (frequency.get(cur) > 1) return acc + frequency.get(cur) - 1;
-    return acc;
-  }, 0);
+  for (let i = n; i < n + m; i++) {
+    if (s.has(arr[i])) answer++;
+  }
+  return answer;
 }
 
-console.log(solution(b, n));
+console.log(solution(b, n, m));

090d6eb1529f5d9095d9a47fef98aa82db203ba8
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-05-01 15:08:16 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,7 +1,8 @@
 def solution(cipher, code):
-    answer = ''
-    n = len(cipher)
-    for i in range(code, n + 1):
-        if i % code == 0:
-            answer += cipher[i - 1]
-    return answer
+    # answer = ''
+    # n = len(cipher)
+    # for i in range(code, n + 1):
+    #     if i % code == 0:
+    #         answer += cipher[i - 1]
+    # return answer
+    return cipher[code-1::code]

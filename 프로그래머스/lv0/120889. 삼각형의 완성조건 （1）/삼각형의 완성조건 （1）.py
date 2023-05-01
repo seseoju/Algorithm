bf69032c989acc0b55f5943ece9da5751e7c7f99
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-05-01 14:46:22 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -1,5 +1,8 @@
+# def solution(sides):
+#     sides.sort()
+#     lastIdx = len(sides) - 1
+#     s = sum(sides[:lastIdx:])
+#     return 1 if sides[lastIdx] < s else 2
+
 def solution(sides):
-    sides.sort()
-    lastIdx = len(sides) - 1
-    s = sum(sides[:lastIdx:])
-    return 1 if sides[lastIdx] < s else 2
+    return 1 if max(sides) < (sum(sides) - max(sides)) else 2

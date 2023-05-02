f30b84b9178a4867fa8f1f5fc88ac2b57668a267
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-05-03 00:25:48 +0900

    [level 0] Title: 숫자 찾기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,3 +1,5 @@
 def solution(num, k):
-    idx = str(num).find(str(k))
-    return idx + 1 if idx != -1 else idx
+    # idx = str(num).find(str(k))
+    # return idx + 1 if idx != -1 else idx
+    
+    return -1 if str(k) not in str(num) else str(num).find(str(k)) + 1

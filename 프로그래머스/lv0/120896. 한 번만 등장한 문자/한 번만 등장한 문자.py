2a7c2aa3fe449da6a50b4c471e9d2a4b0014379d
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-05-04 00:34:55 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,8 +1,9 @@
 def solution(s):
-    answer = ''
-    for i in s:
-        cnt = s.count(i)
-        if cnt == 1:
-            answer += i
-    return ''.join(sorted(answer))
+    # answer = ''
+    # for i in s:
+    #     cnt = s.count(i)
+    #     if cnt == 1:
+    #         answer += i
+    # return ''.join(sorted(answer))
+    return ''.join(sorted([x for x in s if s.count(x) == 1]))
         

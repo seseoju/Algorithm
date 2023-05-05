ded1f14c672ad23013ce9d3adf16e8ccfcaa69b1
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-05-05 11:28:26 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
@@ -1,8 +1,5 @@
 def solution(my_str, n):
     result = []
-    end = len(my_str) // n
-    for i in range(end):
-        result.append(my_str[i*n:(i+1)*n])
-    if (n * end < len(my_str)):
-        result.append(my_str[n*end:])
+    for i in range(0, len(my_str), n):
+        result.append(my_str[i:i+n])
     return result

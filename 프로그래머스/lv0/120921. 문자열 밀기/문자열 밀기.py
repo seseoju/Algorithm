93101f5a44c6f0c6f4de03059f213a36531c3e6d
Hyeonju Seo <79586634+sandwe@users.noreply.github.com>
2023-05-06 12:03:10 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,11 @@
+def solution(A, B):
+    answer = -1
+    t = list(A)
+    
+    for i in range(len(A)):
+        if ''.join(t) == B:
+            answer = i
+            break
+        t.insert(0, t.pop())
+        
+    return answer

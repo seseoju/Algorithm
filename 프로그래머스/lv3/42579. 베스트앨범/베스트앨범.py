60a1bc363a244d0dbcdba8bbc777e89d07a97a44
seo <79586634+sandwe@users.noreply.github.com>
2023-09-07 12:31:18 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,22 +1,27 @@
 def solution(genres, plays):
+    total={} 
+    genres_sort={} 
     answer = []
-    dict = {}
-    count_dict={}
 
-    for i, genre in enumerate(genres):
-        if genre not in dict:
-            dict[genre] = list()
-            count_dict[genre] = 0
+    for i in range(len(genres)):
+        gen = genres[i]
+        if gen not in total:
+            total[gen]= plays[i]
+            genres_sort[gen] = [(plays[i],i)]
+        else:
+            total[gen] +=plays[i]
+            genres_sort[gen].append((plays[i],i))
+    # genres_rank = sorted(total,key = lambda x:x[1], reverse= True)
+    genres_rank = list(map(lambda x: x[0], sorted(total.items(),key = lambda x:x[1], reverse= True)))
 
-        dict[genre].append((plays[i], i))
-        count_dict[genre] += plays[i]
+    print(genres_rank)
+    for gen in genres_rank:
+        genres_sort[gen].sort(key = lambda x: (x[0],-x[1]), reverse=True)
 
-    sorted_genre = sorted(count_dict.items(), key = lambda x:x[1], reverse = True)
- 
-    for genre, _ in sorted_genre:
-        dict[genre].sort(key = lambda x: (-x[0], x[1]))
-        answer.append(dict[genre][0][1])
-        if (len(dict[genre]) > 1):
-            answer.append(dict[genre][1][1])
+    print(genres_sort)
+    for gen in genres_rank:
+        answer.append(genres_sort[gen][0][1])
+        if len(genres_sort[gen]) > 1: #해당 조건 바뀜.
+            answer.append(genres_sort[gen][1][1])
 
     return answer

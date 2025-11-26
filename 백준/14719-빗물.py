af682481a85738e4ccd8a7501e6b85eafa10d025
seseoju <79586634+seseoju@users.noreply.github.com>
2025-11-26 23:30:17 +0900

    Update 14719-빗물.py
    Added an optimized solution for calculating trapped rainwater with O(n) time complexity and O(1) space complexity.

--- 백준/14719-빗물.py
+++ 백준/14719-빗물.py
@@ -1,3 +1,4 @@
+# 1. 시간복잡도: O(n^2), 공간복잡도: O(n)
 h, w = list(map(int, input().split()))
 arr = list(map(int, input().split()))
 
@@ -10,5 +11,28 @@ for i in range(1, w - 1):
   max_h = min(left, right)
   if max_h > arr[i]:
     total += max_h - arr[i]
+
+print(total)
+
+# 2. 시간복잡도: O(n), 공간복잡도: O(1)
+h, w = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+left = 1
+right = w - 2
+
+left_max = arr[left - 1] 
+right_max = arr[right + 1]
+
+total = 0
+while left <= right:
+  if left_max < right_max:
+    total += max(0, left_max - arr[left])
+    left_max = max(left_max, arr[left])
+    left += 1
+  else:
+    total += max(0, right_max - arr[right])
+    right_max = max(right_max, arr[right])
+    right -= 1
 
 print(total)
